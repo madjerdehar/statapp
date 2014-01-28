0352e28c94311cfa29305f1c6aca0d2e894cd158
thywo <thomas.lecocq@ensae.fr>
2014-01-28 16:51:39 +0100

    Update stappa.py

--- stappa.py
+++ stappa.py
@@ -12,12 +12,8 @@ with open("insults.csv", "rb") as f:
     messages=[row[1] for row in csv.reader(f)]
     
     
----    
-exemple SVM:
+# ---    
+# exemple SVM:
 from sklearn import svm
-X = [[0, 0], [1, 1]]
-y = [0, 1]
 clf.fit(X, y)  
-SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3, gamma=0.0, kernel='rbf', max_iter=-1, probability=False, random_state=None,
-shrinking=True, tol=0.001, verbose=False)
----
+
