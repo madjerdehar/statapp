b10691619e3ee07599ac4f24b4b3e77ae0c23df2
thywo <thomas.lecocq@ensae.fr>
2014-01-28 16:44:47 +0100

    Update stappa.py

--- stappa.py
+++ stappa.py
@@ -2,3 +2,8 @@ from scipy.io import mmread
 with open('features-small.matrixMarket.mtx') as f:
     X = mmread(f)
 X.shape
+
+
+import csv
+cr=csv.reader(open("insults.csv","rb")
+y=[int(row[0]) for row in cr]
